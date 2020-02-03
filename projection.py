9a6b3636265a9d52dcdc75b4c2c1631b955c96cb
tung2921 <35050694+tung2921@users.noreply.github.com>
2020-02-03 17:24:44 -0600

    add vectorspace projection

--- projection.py
+++ projection.py
@@ -1,8 +1,9 @@
 import numpy as np
 from numpy import array
 import fractions
+from typing import List
 
-np.set_printoptions(formatter=\
+np.set_printoptions(formatter= \
                         {'all': lambda x: str(fractions.Fraction(x).limit_denominator())})
 
 
@@ -10,18 +11,26 @@ class LinearModels(object):
     def __init__(self):
         pass
 
+    # noinspection PyAttributeOutsideInit
     def projection(self, x: array, y: array) -> array:
         """Return the projection of x onto y
         Input
         -----
-        x,y: [array-like]
+        x,y: [array-like] shape: [n, 1]
         Output
         ------
         return the projection of x onto y"""
-        self.projection_ = (x.T.dot(y) / np.sum(y ** 2)) * y
+        projection_ = (x.T.dot(y) / np.sum(y ** 2)) * y
         # print('The projection is:\n', self.projection_)
-        return self.projection_
+        return projection_
 
-    def GramSchmidt(self):
+    def spaceprojection(self, x: array, Y: List[array]) -> array:
+        """Find the projection of a vector onto a vector space"""
+        projections = np.array([self.projection(x, y) for y in Y])
+        return np.sum(projections)
+
+    def gramschmidt(self, X: List[array]) -> List[array]:
         """Perform GS orthogonalization process"""
+        for idx, val in enumerate(X):
+
         pass
